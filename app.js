f1ae8df20c360867d3c6cbb724d3090ca8d540d2
Eyad Kobatte <eyad.kobatte@meltwater.com>
2019-02-08 17:25:54 +0530

    added contact form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,9 +14,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,3 +41,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('pages/index');
 });
+
+router.post('/contact', (req, res)=>{
+  console.log(req.body);
+  res.render('pages/index', {sentContactForm: true});
+})
 
 module.exports = router;
