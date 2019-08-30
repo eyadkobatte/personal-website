2f4d10876a240fca89cef342559af298753cff27
Eyad Kobatte <ekobatte96@gmail.com>
2019-08-30 16:31:19 +0530

    added purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  module : {
+  module: {
     rules: [
       {
-        test   : /\.scss$/,
-        loader : 'postcss-loader',
+        test: /\.scss$/,
+        loader: 'postcss-loader',
         options: {
-          ident  : 'postcss',
+          ident: 'postcss',
           syntax: 'postcss-scss',
           plugins: () => [
             require('postcss-import'),
             require('tailwindcss'),
             require('autoprefixer'),
+            ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
           ]
         }
       }
